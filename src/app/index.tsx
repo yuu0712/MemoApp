5666ddda9a731adc823e4bce9cdefddfe03ae178
YutoIshikawa <burokkori993@gmail.com>
2025-04-06 16:54:31 +0900

    Markup memo list screen

---
+++ src/app/index.tsx
@@ -0,0 +1,58 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const Index = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Index;
